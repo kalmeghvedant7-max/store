fb29fbe16383e673eb9aaa1e6da02269a511892c
Vedant kalmegh <kalmeghvedant7@gmail.com>
2026-03-15 10:19:02 +0530

    store changes

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println( "my new project on java besicaly i was perpred git github");
+
+        for (int i =0; i<=10; i++){
+
+            if(i==0) {
+
+            }else {
+                System.out.print( 2 * i +" -> :");
+            }
+        }
+    }
+}
---
+++ src/tom.java
@@ -0,0 +1,6 @@
+public class tom {
+
+    public static void main(String[] args) {
+        System.out.println("git git git git");
+    }
+}
